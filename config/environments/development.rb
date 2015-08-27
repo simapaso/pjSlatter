6028ebef29dff55561c9e4e193726fc0d2191284
simapaso3652 <kokopaso_3652@yahoo.co.jp>
2015-08-27 13:16:26 +0900

    second

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-sRails.application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -34,7 +34,7 @@ sRails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+  config.assets.raise_runtime_errors = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
